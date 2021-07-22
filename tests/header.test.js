0f8e7be56076c602fbfc83f66692c94c5a1b23df
Asmovic <asogba007@gmail.com>
2021-07-22 10:26:55 +0100

    Adjust header test file

--- tests/header.test.js
+++ tests/header.test.js
@@ -2,7 +2,6 @@ const Page = require('./helpers/Page');
 
 let page;
 
-
 beforeEach(async () => {
     page = await Page.build();
     await page.goto('http://localhost:3000');
@@ -24,7 +23,7 @@ test('Clicking login button starts 0auth flow', async () => {
     expect(url).toMatch('/accounts\.google\.com');
 });
 
-test('when signed in, shows logout button', async () => {
+test('When signed in, shows logout button', async () => {
     await page.login();
 
     const text = await page.getContentsOf('a[href="/auth/logout"]');
